1bf9171cffa02cc9c0ada9ef6f0e9872eb48380e
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-08-08 16:11:13 -0700

    Time: 17 ms (82.31%), Space: 43.6 MB (90.39%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -2,19 +2,21 @@ class Solution {
     public int mostFrequentEven(int[] nums) {
         HashMap<Integer, Integer> hashmap = new HashMap<>();
         int maxValue = -1;
-        ArrayList<Integer> numList = new ArrayList<>();
+        
         for(int i : nums) {
             if(i % 2 == 0) {
                 hashmap.merge(i, 1, Integer::sum);
             }
         }
+        
         if(hashmap.isEmpty()) {
             return -1;
         }
-        for(int key : hashmap.keySet()) {
-            int count = hashmap.get(key);
-            maxValue = Math.max(maxValue, count);
+        
+        for(int value : hashmap.values()) {
+            maxValue = Math.max(maxValue, value);
         }
+        
         int res = Integer.MAX_VALUE;
         for(int key : hashmap.keySet()) {
             if(hashmap.get(key) == maxValue) {
