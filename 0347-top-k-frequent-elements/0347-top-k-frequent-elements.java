b5c0cb4a835589264873ce5c2a8c97e43e592e03
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-08-08 21:19:07 -0700

    Time: 16 ms (37.69%), Space: 47.8 MB (30.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -10,9 +10,7 @@ import static java.util.Map.Entry.*;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
         int[] array = new int[k];
-        int maxVal = 0;
         
         for(int i : nums) {
             hm.merge(i, 1, Integer::sum);
@@ -26,14 +24,13 @@ class Solution {
             toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
                 LinkedHashMap::new));
         
-        for (Map.Entry<Integer, Integer> entry : sorted.entrySet()) {
+        for (int key : sorted.keySet()) {
             if(k > 0){
-                Integer key = entry.getKey();
+                // Integer key = entry.getKey();
                 array[k-1] = key;
                 k--;
             }  
         }
-        System.out.println(array);
         return array;
     }
 }
