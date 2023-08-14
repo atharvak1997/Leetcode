821700e04eaf0eb3bbb31c0d7f281074006cd10b
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-08-14 11:18:14 -0700

    Time: 16 ms (21.43%), Space: 66.2 MB (6.12%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,30 +1,28 @@
 class Solution {
-    HashMap<Integer, Boolean> hm = new HashMap<>();
-      
-    boolean dfs(int i, int[] nums) {       
-        if(hm.containsKey(i)) {
-            return hm.get(i);
-        }
-        boolean res = false;
-
-        if(i > 0 && nums[i] == nums[i - 1]) {
-            res = res || dfs(i - 2, nums);
-        }
-
-        if(i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[ i - 2]) {
-            res = res || dfs(i - 3, nums);
-        }
-        if(i > 1 && nums[i] - 1 == nums[i - 1] && nums[i - 1] - 1 == nums[ i - 2]) {
-            res = res || dfs(i - 3, nums);
-        }
-
-        hm.put(i, res);
-        return res;
-    }
+        HashMap<Integer, Boolean> hashmap = new HashMap<>();
         public boolean validPartition(int[] nums) {
-
-        hm.put(-1, true);
-        return dfs(nums.length - 1, nums);
+            hashmap.put(-1, true);
+            return dfs(nums, nums.length - 1);
     }
-    
+        boolean dfs(int[] nums, int i) {
+            boolean res = false;
+            if(hashmap.containsKey(i)) {
+                return hashmap.get(i);
+            }
+            
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                res = res || dfs(nums, i - 2);
+            }
+            
+            if(i > 1 && nums[i] - 1 == nums[i - 1] && nums[i - 1] - 1 == nums[i - 2]) {
+                res = res || dfs(nums, i - 3);
+            }
+            
+            if(i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
+                res = res || dfs(nums, i - 3);
+            }
+            
+            hashmap.put(i, res);
+            return res;
+        }
 }
