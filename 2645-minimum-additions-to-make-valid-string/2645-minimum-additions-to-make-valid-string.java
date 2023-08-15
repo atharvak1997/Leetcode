236ebbb0df740f17de64d2800b09ab59535df31d
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-08-15 11:11:08 -0700

    Time: 4 ms (19.15%), Space: 41.9 MB (34.04%) - LeetHub

---
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int addMinimum(String word) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        
+        for(int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+        
+        while(!stack.isEmpty()) {
+            if(!stack.isEmpty() && stack.peek() == 'c') {
+                stack.pop();
+            } else {
+                count++;
+            }
+            
+            if(!stack.isEmpty() && stack.peek() == 'b') {
+                stack.pop();
+            } else {
+                count++;
+            }
+            
+            if(!stack.isEmpty() && stack.peek() == 'a') {
+                stack.pop();
+            } else {
+                count++;
+            }
+            
+        }
+        return count;
+    }
+}
