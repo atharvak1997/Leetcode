0cc37cf31ead0617995858dca41de0b543436fc2
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-08-28 14:34:19 -0700

    Time: 19 ms (16.52%), Space: 65 MB (14.50%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,28 +1,29 @@
 class Solution {
-        HashMap<Integer, Boolean> hashmap = new HashMap<>();
-        public boolean validPartition(int[] nums) {
-            hashmap.put(-1, true);
-            return dfs(nums, nums.length - 1);
+    HashMap<Integer, Boolean> hashmap = new HashMap<>();
+    public boolean validPartition(int[] nums) {
+        
+        hashmap.put(-1, true);
+        return helper(nums, nums.length - 1);
     }
-        boolean dfs(int[] nums, int i) {
-            boolean res = false;
-            if(hashmap.containsKey(i)) {
-                return hashmap.get(i);
-            }
-            
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                res = res || dfs(nums, i - 2);
-            }
-            
-            if(i > 1 && nums[i] - 1 == nums[i - 1] && nums[i - 1] - 1 == nums[i - 2]) {
-                res = res || dfs(nums, i - 3);
-            }
-            
-            if(i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
-                res = res || dfs(nums, i - 3);
-            }
-            
-            hashmap.put(i, res);
-            return res;
+    public boolean helper(int[] nums, int i) {
+        boolean res = false;
+        if(hashmap.containsKey(i)) {
+            return hashmap.get(i);
         }
+        
+        if(i > 0 && nums[i] == nums[i - 1]) {
+            res = res || helper(nums, i - 2);
+        }
+        
+        if(i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
+            res = res || helper(nums, i - 3);
+        }
+        
+        if(i > 1 && nums[i] - 1 == nums[i - 1] && nums[i - 1] - 1 == nums[i - 2]) {
+            res = res || helper(nums, i - 3);
+        }
+        
+        hashmap.put(i, res);
+            return res;
+    }
 }
