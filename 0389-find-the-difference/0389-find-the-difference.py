9e97a6b9346e88deb0c56264bc4df8e91415e5fa
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-09-25 12:58:34 -0700

    Time: 44 ms (40.03%), Space: 16.3 MB (42.33%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,13 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        slen = len(s)
-        tlen = len(t)
-        s = sorted(s)
-        t = sorted(t)
-        i = 0
+        shash = {}
+        thash = {}
 
-        while i < slen:
-            if s[i] != t[i]:
-                return t[i]
-            i += 1
-        return t[i]
+        for chs in s:
+            if chs in shash:
+                shash[chs] += 1
+            else:
+                shash[chs] = 1
+                
+        for cht in t:
+            if cht in thash:
+                thash[cht] += 1
+            else:
+                thash[cht] = 1
+        for cht in thash.keys():
+            if cht not in shash or thash[cht] != shash[cht]:
+                return cht
