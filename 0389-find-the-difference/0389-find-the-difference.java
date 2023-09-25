55ad0c3ffd8a5307abda9bf37e4b8c258b8f55e5
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-09-25 13:10:53 -0700

    Time: 7 ms (17.80%), Space: 41.1 MB (19.84%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,15 +1,23 @@
 class Solution {
     public char findTheDifference(String s, String t) {
+        HashMap<Character, Integer> shash = new HashMap<>();
+        HashMap<Character, Integer> thash = new HashMap<>();
         char[] chs = s.toCharArray();
         char[] cht = t.toCharArray();
-        Arrays.sort(chs);
-        Arrays.sort(cht);
         
-        for(int i = 0; i < s.length(); i++) {
-            if(chs[i] != cht[i]) {
-                return cht[i];
+        for(char ch : chs) {
+            shash.merge(ch, 1, Integer::sum);
+        }
+        for(char ch : cht) {
+            thash.merge(ch, 1, Integer::sum);
+        }
+        
+        for(Map.Entry<Character, Integer> entry : thash.entrySet()) {
+            if(!shash.containsKey(entry.getKey()) || shash.get(entry.getKey()) != entry.getValue()) {
+                return entry.getKey();
             }
         }
-        return cht[t.length() - 1];
+        
+        return 'a';
     }
 }
