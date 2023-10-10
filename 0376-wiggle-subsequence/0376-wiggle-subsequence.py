116d11c4de5485816879cc216405d97591ec6542
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-10-09 20:36:58 -0700

    Time: 48 ms (31.89%), Space: 16.2 MB (95.31%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -3,23 +3,9 @@ class Solution:
         if len(nums) == 1:
             return 1
         count = 1
-        
-        if nums[1] - nums[0] < 0:
-            flag = -1
-            count += 1
-            if len(nums) == 2:
-                return len(nums)
-        elif nums[1] - nums[0] > 0:
-            flag = 1
-            count += 1
-            if len(nums) == 2:
-                return len(nums)
-        else:
-            print("yes")
-            flag = 0
-            if len(nums) == 2:
-                return len(nums) - 1
-        
+        flag = None
+
+
         i = 1
         for i in range(1, len(nums)):
             if flag != -1 and nums[i] - nums[i - 1] < 0:
