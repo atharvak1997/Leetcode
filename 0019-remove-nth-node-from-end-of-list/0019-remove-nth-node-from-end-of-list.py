09602e1e03625405e5c7204ea2a4944bf15b6937
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-10-11 16:17:51 -0700

    Time: 36 ms (81.17%), Space: 16.3 MB (19.78%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,18 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Requirement
+    # We want to return such a linked list which does not have the nth node in it
+    # Given : nth node to remove
+    # Questions : Does Linked List have cycles? 
+                # Will the nth node be out of bounds?
+                # Will head be empty?
+
+# Design
+    # Use a two pointer approach   
+    # One pointer will be the head and another will be the previous node
+    #    1  2  3  4  5 , n = 2 result =  1 2 3 5
+    #  p^      h^
+    # Time complexity = O(n)
+    # Space complexity = O(1)
+
+# Implement
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    def removeNthFromEnd(self, head: Optional[ListNode], n:int) -> Optional[ListNode]:
+        # Dummy will be returned as the answer
         dummy = ListNode(next=head)
-        curr = dummy
+        prev = dummy
 
-        if head:
-            while n:
-                head = head.next
-                n -= 1
-
+        # Offset head by n positions
+        while n > 0:
+            head = head.next
+            n -= 1
+        
+        # Move prev pointer to a location just behind the nth node
         while head:
             head = head.next
-            curr = curr.next
-        curr.next = curr.next.next
+            prev = prev.next
+        # Remove the node next to prev node
+        prev.next = prev.next.next
+
         return dummy.next
+
+# Testing
+#    1  2  4  8  0, n = 3
+#^D         ^p            ^h
+
+        
