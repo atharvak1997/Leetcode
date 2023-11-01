2052fc30f14bbee65658384372b84d4a6126259d
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-11-01 16:21:22 -0700

    Time: 0 ms (100.00%), Space: 40.7 MB (13.03%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> answer1 = new ArrayList<>();
+        ArrayList<Integer> answer2 = new ArrayList<>();
+
+        answer1 = inorder(root1, answer1);
+        
+        answer2 = inorder(root2, answer2);
+        return answer1.equals(answer2);
+    }
+    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arrList) {
+            if(root == null) {
+                return new ArrayList<>();
+            }
+            if(root.left == null && root.right == null) {
+                arrList.add(root.val);
+            }
+            inorder(root.left, arrList);
+            
+            inorder(root.right, arrList);
+
+            return arrList;
+        }
+}
