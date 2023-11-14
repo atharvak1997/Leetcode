c809357865689df4ca73c72da822ae32115586e7
Atharva Kukade <83503996+atharvak1997@users.noreply.github.com>
2023-11-14 14:27:08 -0800

    Time: 679 ms (12.31%), Space: 43.9 MB (18.30%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         HashMap<Character, Integer> hs1 = new HashMap<>();
-        
-
-
         for(int i = 0; i < s1.length(); i++) {
             hs1.merge(s1.charAt(i), 1, Integer::sum);
         }
